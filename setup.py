e175607a2f9b2907e6313328dfd0257aca66cfbc
Shawn Shroyer <shroysha@gmail.com>
2020-04-17 23:45:53 -0400

    first

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='openaigym-2048',
+    name='tf-gym-2048',
     version='',
-    packages=['openaigym_2048', 'openaigym_2048.t48'],
+    packages=['tf_gym_2048', 'tf_gym_2048.t48'],
     url='',
     license='',
     author='Shawn Shroyer',
     author_email='',
-    description='', install_requires=['gym']
+    description=''
 )
